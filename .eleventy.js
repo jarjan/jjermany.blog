2632e45a2c7a93fbb76013f6fc217a2991eb2ec8
Zharzhan Kulmyrza <zharzhan@gmail.com>
2020-01-31 00:37:25 +0100

    Migrate to 11ty (#11)
    * Migrate to 11ty
    
    * fixed site

---
+++ .eleventy.js
@@ -0,0 +1,52 @@
+const { DateTime, Settings } = require("luxon");
+const util = require("util");
+
+Settings.defaultLocale = "ru";
+
+module.exports = function(eleventyConfig) {
+  // Layout aliases for convenience
+  eleventyConfig.addLayoutAlias("default", "layouts/base.njk");
+  eleventyConfig.addLayoutAlias("conf", "layouts/conf.njk");
+
+  // a debug utility
+  eleventyConfig.addFilter("dump", obj => {
+    return util.inspect(obj);
+  });
+
+  eleventyConfig.addFilter("htmlDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: "utc"
+    }).toFormat("y-MM-dd");
+  });
+
+  // Grab excerpts and sections from a file
+  eleventyConfig.addFilter("section", require("./src/utils/section.js"));
+
+  // compress and combine js files
+  eleventyConfig.addFilter("jsmin", require("./src/utils/minify-js.js"));
+
+  // minify the html output when running in prod
+  if (process.env.NODE_ENV == "production") {
+    eleventyConfig.addTransform(
+      "htmlmin",
+      require("./src/utils/minify-html.js")
+    );
+  }
+
+  // Static assets to pass through
+  eleventyConfig.addPassthroughCopy("./src/site/fonts");
+  eleventyConfig.addPassthroughCopy("./src/site/images");
+  eleventyConfig.addPassthroughCopy("./src/site/css");
+
+  return {
+    dir: {
+      input: "src/site",
+      includes: "_includes",
+      output: "dist"
+    },
+    passthroughFileCopy: true,
+    templateFormats: ["njk", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
--- themes/einfach-theme/README.md
+++
@@ -1,1 +0,0 @@
-# einfach-theme
